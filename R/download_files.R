e7061562d77efd6e4c8099a24722e2b26b5e1db9
Jay Hughes (tier0) <tier0\jay.hughes@ec2amaz-ivsmyb.tier0.prd.thf.aws>
2024-10-17 14:44:30 +0000

    rename arg for clarity

--- R/download_files.R
+++ R/download_files.R
@@ -1,49 +1,49 @@
-## Download all files in links_out_df
-dl_files <- function(links_out_df){
+## Download all files in data
+dl_files <- function(data){
   
-  for (k in 1:nrow(links_out_df)){
+  for (k in 1:nrow(data)){
     
     #Download Full CSV in workbench
-    download(as.character(links_out_df$full.csv.link[k]),
-             dest=paste0("RTT_temp_data/temp files/",links_out_df$month[k],".zip"), mode="wb")
+    download(as.character(data$full.csv.link[k]),
+             dest=paste0("RTT_temp_data/temp files/",data$month[k],".zip"), mode="wb")
     #Unzip Full CSV in workbench
-    unzip(paste0("RTT_temp_data/temp files/",links_out_df$month[k],".zip"),
-          exdir = paste0("RTT_temp_data/temp files/",links_out_df$month[k]))
+    unzip(paste0("RTT_temp_data/temp files/",data$month[k],".zip"),
+          exdir = paste0("RTT_temp_data/temp files/",data$month[k]))
     #Delete zip file
-    file.remove(paste0("RTT_temp_data/temp files/",links_out_df$month[k],".zip"))
+    file.remove(paste0("RTT_temp_data/temp files/",data$month[k],".zip"))
     
     #Download New Providers
-    download(as.character(links_out_df$providers.link.new[k]),
+    download(as.character(data$providers.link.new[k]),
              dest=paste0("RTT_temp_data/temp files/",
-                         paste(links_out_df$month[k]),"/",
-                         links_out_df$month[k],"-newproviders.", 
-                         tools::file_ext(links_out_df$providers.link.new[k])),
+                         paste(data$month[k]),"/",
+                         data$month[k],"-newproviders.", 
+                         tools::file_ext(data$providers.link.new[k])),
              mode="wb")
     
     #Download Admitted Providers
-    download(as.character(links_out_df$providers.link.adm[k]),
+    download(as.character(data$providers.link.adm[k]),
              dest=paste0("RTT_temp_data/temp files/",
-                         paste(links_out_df$month[k]),"/",
-                         links_out_df$month[k],"-providers-admitted.",
-                         tools::file_ext(links_out_df$providers.link.adm[k])),
+                         paste(data$month[k]),"/",
+                         data$month[k],"-providers-admitted.",
+                         tools::file_ext(data$providers.link.adm[k])),
              mode="wb")
     
     #Download Non-Admitted Providers
-    download(as.character(links_out_df$providers.link.nonadm[k]),
+    download(as.character(data$providers.link.nonadm[k]),
              dest=paste0("RTT_temp_data/temp files/",
-                         paste(links_out_df$month[k]),"/",
-                         links_out_df$month[k],"-providers-nonadmitted.",
-                         tools::file_ext(links_out_df$providers.link.nonadm[k])), 
+                         paste(data$month[k]),"/",
+                         data$month[k],"-providers-nonadmitted.",
+                         tools::file_ext(data$providers.link.nonadm[k])), 
              mode="wb")
     
     ### Incomplete providers
     
     #Download Incomplete Providers
-    download(as.character(links_out_df$providers.link.incomp[k]),
+    download(as.character(data$providers.link.incomp[k]),
              dest=paste0("RTT_temp_data/temp files/",
-                         paste(links_out_df$month[k]),"/",
-                         links_out_df$month[k],"-providers-incomplete.",
-                         tools::file_ext(links_out_df$providers.link.incomp[k])),
+                         paste(data$month[k]),"/",
+                         data$month[k],"-providers-incomplete.",
+                         tools::file_ext(data$providers.link.incomp[k])),
              mode="wb") 
   }
 }

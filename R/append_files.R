eea6e56eb5caf204f3852c56a92e598b224972a3
Jay Hughes (tier0) <tier0\jay.hughes@ec2amaz-ivsmyb.tier0.prd.thf.aws>
2024-10-24 10:01:45 +0000

    fix error

--- R/append_files.R
+++ R/append_files.R
@@ -1,50 +1,45 @@
 ## Append all IS provider files into one
-append_IS <- function(data){
+append_IS <- function(file_locs){
   
-  for (s in 1:nrow(data)){
+  ## get months from file locs
+  unique_month <- substr(file_locs, 1, 5) |>
+                  unique()
+  
+  for (s in 1:length(unique_month)){
     
-    init <- paste0('RTT_temp_data/temp files/', data$month[s], '/', data$month[s])
+    init <- paste0('RTT_temp_data/temp_files/', unique_month[s], '/')
+    month_files <- paste0(init, list.files(init))
     
     #Open all provider files for one month and append
-    incomplete <- read_excel(paste0(init,
-                                    '-providers-incomplete.',
-                                    file_ext(data$providers.link.incomp[s])),
+    incomplete <- read_excel(grep('-providers-incomplete', month_files, value = TRUE),
                              sheet = 'IS Provider',
                              skip = 13)
     
-    incompleteDTA <- read_excel(paste0(init,
-                                       '-providers-incomplete.',
-                                       file_ext(data$providers.link.incomp[s])),
+    incompleteDTA <- read_excel(grep('-providers-incomplete', month_files, value = TRUE),
                                 sheet = 'IS Provider with DTA',
                                 skip = 13)
     
-    new_provider <- read_excel(paste0(init,
-                                      '-newproviders.',
-                                      file_ext(data$providers.link.new[s])),
+    new_provider <- read_excel(grep('-newproviders', month_files, value = TRUE),
                                sheet = 'IS Provider',
                                skip = 13)
     
-    adm_provider <- read_excel(paste0(init,
-                                      '-providers-admitted.',
-                                      file_ext(data$providers.link.adm[s])),
+    adm_provider <- read_excel(grep('-providers-admitted', month_files, value = TRUE),
                                sheet = 'IS Provider',
                                skip = 13)
     
-    nonadm_provider <- read_excel(paste0(init,
-                                         '-providers-nonadmitted.',
-                                         file_ext(data$providers.link.nonadm[s])),
+    nonadm_provider <- read_excel(grep('-providers-nonadmitted', month_files, value = TRUE),
                                   sheet = 'IS Provider',
                                   skip = 13)
     
     #IS providers for that month
     
-    codes <- c(new_provider$`Provider Code`,adm_provider$`Provider Code`,nonadm_provider$`Provider Code`,
-               incomplete$`Provider Code`,incompleteDTA$`Provider Code`)
-    names <- c(new_provider$`Provider Name`,adm_provider$`Provider Name`,nonadm_provider$`Provider Name`,
-               incomplete$`Provider Name`,incompleteDTA$`Provider Name`)
-    region <- c(new_provider$`Region Code`,adm_provider$`Region Code`,nonadm_provider$`Region Code`,
-                incomplete$`Region Code`,incompleteDTA$`Region Code`)
-    summary_month <- data.frame(monthyr=rep(as.character(data$month[s]),length(codes)),codes,names,region)
+    codes <- c(new_provider$`Provider Code`, adm_provider$`Provider Code`, nonadm_provider$`Provider Code`,
+               incomplete$`Provider Code`, incompleteDTA$`Provider Code`)
+    names <- c(new_provider$`Provider Name`, adm_provider$`Provider Name`, nonadm_provider$`Provider Name`,
+               incomplete$`Provider Name`, incompleteDTA$`Provider Name`)
+    region <- c(new_provider$`Region Code`, adm_provider$`Region Code`, nonadm_provider$`Region Code`,
+                incomplete$`Region Code`, incompleteDTA$`Region Code`)
+    summary_month <- data.frame(monthyr=rep(unique_month[s],length(codes)),codes,names,region) |> unique()
     
     #Successively append files
     
@@ -58,10 +53,13 @@ append_IS <- function(data){
   #Remove duplicates
   is_prov <- storage[!duplicated(storage), ]
   
-  #Save
-  fwrite(is_prov, file = 'RTT_temp_data/IS_prov.csv')
-  return('RTT_temp_data/IS_prov.csv') ## target name:app_IS
-                                      ## is location of is_prov file
+  # filename
+  name <- 'RTT_temp_data/IS_prov.csv'
+  
+  #Save - include time in name to check in targets
+  fwrite(is_prov, file = name)
+  return(name) ## target name:app_IS
+               ## is location of is_prov file (will need to clear out the others?)
 }
 
 #### Get all NHS provider locations / region

a682fd6e3b65f4ce5dc2576ac8085e4596217335
Jay Hughes (tier0) <tier0\jay.hughes@ec2amaz-ivsmyb.tier0.prd.thf.aws>
2024-10-25 15:36:03 +0000

    create new datasets for charts

---
+++ R/process_rtt.R
@@ -0,0 +1,30 @@
+
+
+process_rtt <- function(combined_dataset,
+                        specialty = 'Total',
+                        type){
+  
+  all_months <- get_months(combined_dataset)
+  res <- list()
+  j <- 1
+  
+  for (i in all_months){
+    
+    res[[j]] <- dashboard_stats_ccg(combined_dataset,
+                                    monthyear = i,
+                                    specialty,
+                                    type = type,
+                                    independent = 0)
+    res[[j+1]] <- dashboard_stats_ccg(combined_dataset,
+                                    monthyear = i,
+                                    specialty,
+                                    type = type,
+                                    independent = 1)
+    j <- j + 2
+  }
+ 
+  return(
+    rbindlist(res)
+  )
+  
+}
